a947fc8ecbda15d42db3b4418b17b1bd008c425a
Ryan Fleming <ryanfleming93@aol.com>
2022-10-06 16:47:06 +0100

    website update

--- app.js
+++ app.js
@@ -1,44 +1,41 @@
-
-
-
 /* Lazy Loading Script */
 
-document.addEventListener("DOMContentLoaded", function() {
-    var lazyloadImages = document.querySelectorAll("img.lazy");    
-    var lazyloadThrottleTimeout;
-    
-    function lazyload () {
-      if(lazyloadThrottleTimeout) {
-        clearTimeout(lazyloadThrottleTimeout);
-      }    
-      
-      lazyloadThrottleTimeout = setTimeout(function() {
-          var scrollTop = window.pageYOffset;
-          lazyloadImages.forEach(function(img) {
-              if(img.offsetTop < (window.innerHeight + scrollTop)) {
-                img.src = img.dataset.src;
-                img.classList.remove('lazy');
-              }
-          });
-          if(lazyloadImages.length == 0) { 
-            document.removeEventListener("scroll", lazyload);
-            window.removeEventListener("resize", lazyload);
-            window.removeEventListener("orientationChange", lazyload);
-          }
-      }, 20);
+document.addEventListener("DOMContentLoaded", function () {
+  var lazyloadImages = document.querySelectorAll("img.lazy");
+  var lazyloadThrottleTimeout;
+
+  function lazyload() {
+    if (lazyloadThrottleTimeout) {
+      clearTimeout(lazyloadThrottleTimeout);
     }
-    
-    document.addEventListener("scroll", lazyload);
-    window.addEventListener("resize", lazyload);
-    window.addEventListener("orientationChange", lazyload);
-  });
+
+    lazyloadThrottleTimeout = setTimeout(function () {
+      var scrollTop = window.pageYOffset;
+      lazyloadImages.forEach(function (img) {
+        if (img.offsetTop < (window.innerHeight + scrollTop)) {
+          img.src = img.dataset.src;
+          img.classList.remove('lazy');
+        }
+      });
+      if (lazyloadImages.length == 0) {
+        document.removeEventListener("scroll", lazyload);
+        window.removeEventListener("resize", lazyload);
+        window.removeEventListener("orientationChange", lazyload);
+      }
+    }, 20);
+  }
+
+  document.addEventListener("scroll", lazyload);
+  window.addEventListener("resize", lazyload);
+  window.addEventListener("orientationChange", lazyload);
+});
 
 
 
 
-  // * website opening multiple URL's for viewing projects * //
-  $('a.allprojects').click(function(e) {
-    e.preventDefault();
-    window.open('http://www.yoururl1.com');
-    window.open('http://www.yoururl2.com');
+// * website opening multiple URL's for viewing projects * //
+$('a.allprojects').click(function (e) {
+  e.preventDefault();
+  window.open('http://www.yoururl1.com');
+  window.open('http://www.yoururl2.com');
 });
